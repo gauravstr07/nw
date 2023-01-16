5820d367760b1a224379dbd194e02dc34665a44c
Gaurav Sutar <gauravstr05@gmail.com>
2023-01-16 12:15:35 +0530

    Fira Code

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const index = () => {
+  function app() {}
+  app();
+
+  return (
+    <div className="container">
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default index;
