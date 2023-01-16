bc1e5f41389631c9d963b13bc4e592025c236fd0
Gaurav Sutar <gauravstr05@gmail.com>
2023-01-16 14:05:21 +0530

    Hy

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-import React from "react";
+import React from 'react'
 
-const index = () => {
-  function app() {}
-  app();
-
+function index() {
   return (
-    <div className="container">
-      <h1>hello world</h1>
-    </div>
-  );
-};
+    <div>index</div>
+  )
+}
 
-export default index;
+export default index
